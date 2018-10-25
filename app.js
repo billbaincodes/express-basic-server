5e0fd8bb25da617d58428ad20c9886001d75dec5
bill <williamandrewbain@gmail.com>
2018-10-25 11:06:15 -0600

    made port dynamic with env variable

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-let port = 3000
+let port = process.env.PORT || 3000
 const bodyParser = require('body-parser')
 const charactersRoutes = require('./routes/characters')
 
