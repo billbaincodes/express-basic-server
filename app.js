c23a722c140da8fa35b7237f16e63ceb150a4342
bill <williamandrewbain@gmail.com>
2018-10-25 11:10:51 -0600

    replaced string with template string

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use('/characters', charactersRoutes)
 
 
 app.use((req, res, next) => {
-  res.status(404).json({ error: { message: 'What\'d you do?!' }, status: 404})
+  res.status(404).json({ error: { message: `What'd you do?!` }, status: 404})
 })
 
 // eslint-disable-next-line
