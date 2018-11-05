052116dcb2745d8e0c0dffd2599f6c413cf6354c
bill <williamandrewbain@gmail.com>
2018-11-05 11:43:42 -0700

    update knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,11 +1,14 @@
 // Update with your config settings.
 module.exports = {
+
   development: {
     client: 'pg',
     connection: 'postgres://localhost/characters'
   },
+
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL + '?ssl=true'
   }
+
 }
